710f37552a3f681dca9df86a85ead1f983706821
Karvari <mzphitt@gmail.com>
2015-02-25 17:58:11 -0800

    edit form for character creation

--- services/CharactersFactory.js
+++ services/CharactersFactory.js
@@ -3,11 +3,11 @@ galacticJump.factory('CharactersFactory', function CharactersFactory() {
   factory.flurps = [];
   factory.customize = function () {
     factory.flurps.push({ name: factory.flurpName, hair: factory.hairColor, eyes: factory.eyeColor, howTall: factory.howTall, weight: factory.weight })
+    factory.flurpName = null;
     factory.hairColor = null;
     factory.eyeColor = null;
     factory.howTall = null;
     factory.weight = null;
-debugger;    
   };
   return factory;
 });
